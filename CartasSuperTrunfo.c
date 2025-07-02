a4659b1a10b5bffcc70bf280f643c1caf859c9d7
THprogamador <zzthiago5@gmail.com>
2025-07-02 02:11:51 +0000

    progama finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,8 +3,7 @@
 int main() {
 
  printf("desafio super trunfo\n\n");
-
- printf("digite informções do primeiro país\n\n");
+ printf("digite informções da primeira carta\n\n");
 
 char estado[20];
 char codigo_da_carta[10];
@@ -41,7 +40,46 @@ printf("nome da cidade: %s\n",nome_da_cidade);
 printf("numero da população: %d\n",população);
 printf("area: %f\n",area);
 printf("PIB: %f\n",PIB);
-printf("numero de ponto turistico: %d\n",numero_ponto_turistico);
+printf("numero de ponto turistico: %d\n\n",numero_ponto_turistico);
+
+printf("agora digite informações da carta 2\n\n");
+
+char estado2[20];
+char codigo_da_carta2[10];
+char nome_da_cidade2[20];
+int população2;
+float area2;
+float PIB2;
+int numero_ponto_turistico2;
+
+printf("digite estado: \n");
+scanf("%s", estado2);
+
+printf("digite codigo da carta: \n");
+scanf("%s", codigo_da_carta2);
+
+printf("digite nome da cidade: \n");
+scanf("%s", nome_da_cidade2);
+
+printf("digite população: \n");
+scanf("%d", &população2);
+
+printf("digite area: \n");
+scanf("%f", &area2);
+
+printf("digite PIB: \n");
+scanf("%f", &PIB2);
+
+printf("digite numero ponto turistico: \n");
+scanf("%d", &numero_ponto_turistico2);
+
+printf("nome do estado: %s\n",estado2);
+printf("codigo da carta: %s\n",codigo_da_carta2);
+printf("nome da cidade: %s\n",nome_da_cidade2);
+printf("numero da população: %d\n",população2);
+printf("area: %f\n",area2);
+printf("PIB: %f\n",PIB2);
+printf("numero de ponto turistico: %d\n",numero_ponto_turistico2);
 
 
     return 0;
